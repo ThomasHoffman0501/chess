3bc43be6663ce9f4badfa8955cea79dcd78287ca
ThomasHoffman0501 <thomashoffman0501@gmail.com>
2024-01-25 16:26:41 -0700

    Debugged Rook Moves (1 hr)

--- shared/src/main/java/chess/RookMoveSet.java
+++ shared/src/main/java/chess/RookMoveSet.java
@@ -6,39 +6,38 @@ public class RookMoveSet {
     public Collection<ChessMove> getValidMoves(ChessBoard board, ChessPosition currentPosition, ChessGame.TeamColor pieceColor) {
         Collection<ChessMove> validMoves = new HashSet<>();
 
-        for (int i = 1 ; i <= 8; i++) {
-            if (i != currentPosition.getRow()) {
-                ChessPosition openPosition = new ChessPosition(i, currentPosition.getColumn());
-                if (board.getPiece(openPosition)==null){
-                    validMoves.add(new ChessMove(currentPosition, new ChessPosition(i, currentPosition.getColumn()), null));
-                } else if (checkEnemy(board, openPosition, pieceColor)) {
-                    validMoves.add(new ChessMove(currentPosition, new ChessPosition(i, currentPosition.getColumn()), null));
-                    break;
-                } else { break; }
-            }
-            // for (int i = 1; i <= -8; i++) {
-            if (i != currentPosition.getColumn()) {
-                ChessPosition openPosition = new ChessPosition(i, currentPosition.getRow());
-                if (board.getPiece(openPosition)==null){
-                    validMoves.add(new ChessMove(currentPosition, new ChessPosition(currentPosition.getRow(), i), null));
-                } else if (checkEnemy(board, openPosition, pieceColor)) {
-                    validMoves.add(new ChessMove(currentPosition, new ChessPosition(currentPosition.getRow(), i), null));
-                    break;
-                } else { break; }
-            }
-        }
+        int currentRow = currentPosition.getRow();
+        int currentColumn = currentPosition.getColumn();
+
+        addStraightMoves(validMoves, board, currentRow, currentColumn, 1, 0, pieceColor); // Left
+        addStraightMoves(validMoves, board, currentRow, currentColumn, -1, 0, pieceColor); // Right
+        addStraightMoves(validMoves, board, currentRow, currentColumn, 0, 1, pieceColor); // Up
+        addStraightMoves(validMoves, board, currentRow, currentColumn, 0, -1, pieceColor); // Down
+
         return validMoves;
-        // If (pieceColor = ChessMove(currentPosition, new ChessPosition(i,currentPosition, ChessGame.TeamColor pieceColor) {
-        // return validMoves
-        // if (pieceColor != ChessMove(currentPosition, new ChessPosition(i,currentPosition, ChessGame.TeamColor pieceColor) {
-        // break;
     }
 
-    private boolean checkEnemy(ChessBoard board, ChessPosition openPosition, ChessGame.TeamColor mycolor) {
-        if (board.getPiece(openPosition).getTeamColor() != mycolor) {
-            return true;
-        } else {
-            return false; // This is a piece of the same color (Not an enemy)
+    private static void addStraightMoves(Collection<ChessMove> validMoves, ChessBoard board, int currentRow, int currentColumn, int rowDirection, int colDirection, ChessGame.TeamColor color) {
+        int newRow = currentRow + rowDirection;
+        int newColumn = currentColumn + colDirection;
+
+        while (ChessBoard.isValidPosition(newRow, newColumn)) {
+            ChessPiece pieceAtNewPosition = board.getPiece(new ChessPosition(newRow, newColumn));
+
+            if (pieceAtNewPosition == null) {
+                // Add the move when the target square is empty
+                validMoves.add(new ChessMove(new ChessPosition(currentRow, currentColumn),
+                        new ChessPosition(newRow, newColumn), null));
+            } else if (pieceAtNewPosition.getTeamColor() != color) {
+                // Add the move when there is an opponent's piece
+                validMoves.add(new ChessMove(new ChessPosition(currentRow, currentColumn),
+                        new ChessPosition(newRow, newColumn), null));
+                break;
+            } else {break;}
+
+
+            newRow += rowDirection;
+            newColumn += colDirection;
         }
     }
 }
