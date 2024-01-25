eefb17454a3406804b1bbe545e74114c26df2fa5
ThomasHoffman0501 <thomashoffman0501@gmail.com>
2024-01-25 15:48:58 -0700

    BishopMoves with working tests (3hrs)

--- shared/src/main/java/chess/BishopMoveSet.java
+++ shared/src/main/java/chess/BishopMoveSet.java
@@ -1,41 +1,41 @@
 package chess;
-
 import java.util.Collection;
 import java.util.HashSet;
 public class BishopMoveSet {
-    public static Collection<ChessMove> getValidMoves(ChessBoard board, ChessPosition currentPosition) {
-        Collection<ChessMove> validMoves = new HashSet <>();
+
+    public static Collection<ChessMove> getValidMoves(ChessBoard board, ChessPosition currentPosition, ChessGame.TeamColor pieceColor) {
+        HashSet<ChessMove> validMoves = new HashSet <>();
 
         int currentRow = currentPosition.getRow();
         int currentColumn = currentPosition.getColumn();
 
-        addDiagonalMoves(validMoves, board, currentRow, currentColumn, 1, 1); // Diagonal: DR
-        addDiagonalMoves(validMoves, board, currentRow, currentColumn, -1, 1); // Diagonal: UR
-        addDiagonalMoves(validMoves, board, currentRow, currentColumn, 1, -1); // Diagonal: DL
-        addDiagonalMoves(validMoves, board, currentRow, currentColumn, -1, -1); // Diagonal: UL
+        addDiagonalMoves(validMoves, board, currentRow, currentColumn, 1, 1, pieceColor); // Diagonal: DR
+        addDiagonalMoves(validMoves, board, currentRow, currentColumn, -1, 1, pieceColor); // Diagonal: UR
+        addDiagonalMoves(validMoves, board, currentRow, currentColumn, 1, -1, pieceColor); // Diagonal: DL
+        addDiagonalMoves(validMoves, board, currentRow, currentColumn, -1, -1, pieceColor); // Diagonal: UL
 
         return validMoves;
     }
 
-    private static void addDiagonalMoves(Collection<ChessMove> validMoves, ChessBoard board, int currentRow, int currentColumn, int rowDirection, int colDirection) {
+    private static void addDiagonalMoves(Collection<ChessMove> validMoves, ChessBoard board, int currentRow, int currentColumn, int rowDirection, int colDirection, ChessGame.TeamColor color) {
         int newRow = currentRow + rowDirection;
         int newColumn = currentColumn + colDirection;
 
         while (ChessBoard.isValidPosition(newRow, newColumn)) {
             ChessPiece pieceAtNewPosition = board.getPiece(new ChessPosition(newRow, newColumn));
+
             if (pieceAtNewPosition == null) {
                 // Add the move when the target square is empty
                 validMoves.add(new ChessMove(new ChessPosition(currentRow, currentColumn),
                         new ChessPosition(newRow, newColumn), null));
-            } else {
-                if (!pieceAtNewPosition.getTeamColor().equals(board.getPiece(new ChessPosition(currentRow, currentColumn)).getTeamColor())) {
+            } else if (pieceAtNewPosition.getTeamColor() != color) {
                     // Add the move when there is an opponent's piece
                     validMoves.add(new ChessMove(new ChessPosition(currentRow, currentColumn),
                             new ChessPosition(newRow, newColumn), null));
-                }
-                // Break after capturing an opponent's piece
-                break;
-            }
+                    break;
+                } else {break;}
+
+
             newRow += rowDirection;
             newColumn += colDirection;
         }
