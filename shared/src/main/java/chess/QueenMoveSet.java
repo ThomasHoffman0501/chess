83eaebc044593794617ac5ae70887ff6ea0e4ee7
ThomasHoffman0501 <thomashoffman0501@gmail.com>
2024-01-25 16:31:20 -0700

    Debugged Queen Moves (30 min)

--- shared/src/main/java/chess/QueenMoveSet.java
+++ shared/src/main/java/chess/QueenMoveSet.java
@@ -2,63 +2,75 @@ package chess;
 import java.util.Collection;
 import java.util.HashSet;
 public class QueenMoveSet {
-    public static Collection<ChessMove> getValidMoves(ChessBoard board, ChessPosition currentPosition, ChessGame.TeamColor pieceColor) {
-        HashSet<ChessMove> validMoves = new HashSet<>();
+    public Collection<ChessMove> getValidMoves(ChessBoard board, ChessPosition currentPosition, ChessGame.TeamColor pieceColor) {
+        Collection<ChessMove> validMoves = new HashSet<>();
 
-        // Rook Moves
-        for (int i = 1; i <= 8; i++) {
-            if (i != currentPosition.getRow()) {
-                validMoves.add(new ChessMove(currentPosition, new ChessPosition(i, currentPosition.getColumn()), null));
-            }
-            if (i != currentPosition.getColumn()) {
-                validMoves.add(new ChessMove(currentPosition, new ChessPosition(currentPosition.getRow(), i), null));
-            }
-        }
-
-        // Bishop Moves
         int currentRow = currentPosition.getRow();
         int currentColumn = currentPosition.getColumn();
 
-        addDiagonalMoves(validMoves, board, currentRow, currentColumn, 1, 1); // Diagonal: DR
-        addDiagonalMoves(validMoves, board, currentRow, currentColumn, -1, 1); // Diagonal: UR
-        addDiagonalMoves(validMoves, board, currentRow, currentColumn, 1, -1); // Diagonal: DL
-        addDiagonalMoves(validMoves, board, currentRow, currentColumn, -1, -1); // Diagonal: UL
+        // Rook Moves
+        addStraightMoves(validMoves, board, currentRow, currentColumn, 1, 0, pieceColor); // Left
+        addStraightMoves(validMoves, board, currentRow, currentColumn, -1, 0, pieceColor); // Right
+        addStraightMoves(validMoves, board, currentRow, currentColumn, 0, 1, pieceColor); // Up
+        addStraightMoves(validMoves, board, currentRow, currentColumn, 0, -1, pieceColor); // Down
+
+        // Bishop Moves
+        addDiagonalMoves(validMoves, board, currentRow, currentColumn, 1, 1, pieceColor); // Diagonal: DR
+        addDiagonalMoves(validMoves, board, currentRow, currentColumn, -1, 1, pieceColor); // Diagonal: UR
+        addDiagonalMoves(validMoves, board, currentRow, currentColumn, 1, -1, pieceColor); // Diagonal: DL
+        addDiagonalMoves(validMoves, board, currentRow, currentColumn, -1, -1, pieceColor); // Diagonal: UL
 
         return validMoves;
     }
 
-    // Bishop MoveSet
-    // Like Queen, Bishop can move to any empty square, capture the enemy's piece and then end movement.
-    private static void addDiagonalMoves(Collection<ChessMove> validMoves, ChessBoard board, int currentRow, int currentColumn, int rowDirection, int colDirection) {
+    // Rook Capturing and Blocking Enemy
+    private static void addStraightMoves(Collection<ChessMove> validMoves, ChessBoard board, int currentRow, int currentColumn, int rowDirection, int colDirection, ChessGame.TeamColor color) {
         int newRow = currentRow + rowDirection;
         int newColumn = currentColumn + colDirection;
 
-        while (ChessBoard.isValidPosition(newRow,newColumn)) {
+        while (ChessBoard.isValidPosition(newRow, newColumn)) {
             ChessPiece pieceAtNewPosition = board.getPiece(new ChessPosition(newRow, newColumn));
+
             if (pieceAtNewPosition == null) {
+                // Add the move when the target square is empty
                 validMoves.add(new ChessMove(new ChessPosition(currentRow, currentColumn),
                         new ChessPosition(newRow, newColumn), null));
-            } else {
-                if (!pieceAtNewPosition.getTeamColor().equals(board.getPiece(new ChessPosition(currentRow, currentColumn)).getTeamColor())) {
-                    validMoves.add(new ChessMove(new ChessPosition(currentRow, currentColumn),
-                            new ChessPosition(newRow,newColumn),null));
-                }
+            } else if (pieceAtNewPosition.getTeamColor() != color) {
+                // Add the move when there is an opponent's piece
+                validMoves.add(new ChessMove(new ChessPosition(currentRow, currentColumn),
+                        new ChessPosition(newRow, newColumn), null));
                 break;
-            }
+            } else {break;}
+
+
             newRow += rowDirection;
             newColumn += colDirection;
         }
     }
 
-private boolean checkEnemy(ChessBoard board, ChessPosition openPosition, ChessGame.TeamColor mycolor) {
-    if (board.getPiece(openPosition) == null) {
-        return true;
+    // Bishop Capturing and Blocking Enemy
+    private static void addDiagonalMoves(Collection<ChessMove> validMoves, ChessBoard board, int currentRow, int currentColumn, int rowDirection, int colDirection, ChessGame.TeamColor color) {
+        int newRow = currentRow + rowDirection;
+        int newColumn = currentColumn + colDirection;
+
+        while (ChessBoard.isValidPosition(newRow, newColumn)) {
+            ChessPiece pieceAtNewPosition = board.getPiece(new ChessPosition(newRow, newColumn));
+
+            if (pieceAtNewPosition == null) {
+                // Add the move when the target square is empty
+                validMoves.add(new ChessMove(new ChessPosition(currentRow, currentColumn),
+                        new ChessPosition(newRow, newColumn), null));
+            } else if (pieceAtNewPosition.getTeamColor() != color) {
+                // Add the move when there is an opponent's piece
+                validMoves.add(new ChessMove(new ChessPosition(currentRow, currentColumn),
+                        new ChessPosition(newRow, newColumn), null));
+                break;
+            } else {break;}
+
+
+            newRow += rowDirection;
+            newColumn += colDirection;
+        }
     }
-    if (board.getPiece(openPosition).getTeamColor() != mycolor) {
-        return true;
-    } else {
-        return false; // This is a piece of the same color (Not an enemy)
-    }
-}
 }
 
